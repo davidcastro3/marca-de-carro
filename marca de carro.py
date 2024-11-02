ab86d8709ab01464d447ebfb85351c7637f2c6b9
davidcastro3 <davidcastrinho3@icloud.com>
2024-11-02 12:51:32 +0000

    Add files via upload

---
+++ marca de carro.py
@@ -0,0 +1,9 @@
+marca=input("qual é a marca do carro?")
+if marca in 'mercedes':
+    print("a marca é alemã")
+elif marca in 'ford':
+    print("a marca é americana")
+elif marca in 'peugeot':
+    print("a marca é francesa")
+elif marca in 'ford':
+    print("a marca é japonesa")
